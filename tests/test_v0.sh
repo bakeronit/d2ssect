52a64c6ba44716202c47eedec80b35e2b6d58c73
bakeronit <bakerzj36@gmail.com>
2022-03-02 17:56:20 +1000

    add bats tests

--- tests/test_v0.sh
+++ tests/test_v0.sh
@@ -1,15 +1,37 @@
-#test_runv0.sh
 #!/usr/bin/env bats
-### this is a naive test for origin workflow which will run the codes step-by-step and generate some intermedia files.
+
+setup() {
+    load 'test_helper/bats-support/load'
+    load 'test_helper/bats-assert/load'
+
+    DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
+    PATH="$DIR/../:$PATH"
+    TEST_TEMP_DIR="$(mktemp -d)"
+    source alignment_free_tool_workflow.sh
+    cd $DIR/..
+    jellyfish_count $TEST_TEMP_DIR
+    jellyfish_dump $TEST_TEMP_DIR
+    cal_d2s $TEST_TEMP_DIR
+    generate_matrix $TEST_TEMP_DIR
+}
+
+teardown() {
+    rm -rf "$TEST_TEMP_DIR"
+}
+
+check_charfreq() {
+    cmp data/DI-1-1_S6.CharFreq ${TEST_TEMP_DIR}/DI-1-1_S6.CharFreq
+}
+
+#@test "the output from jellyfish should be the same, this is a binary compressed file" 
 
 
+@test "the output from charfreq should be the same, text file" {
+    run check_charfreq
+    [ "$output" = "" ] 
+}
 
-@test "the output from jellyfish should be the same, this is a binary compressed file"
+#@test "the output from d2s, a single text file"
 
-
-@test "the output from charfreq should be the same, text file"
-
-@test "the output from d2s, a single text file"
-
-@test "the final matrix, text file"
+#@test "the final matrix, text file"
 
