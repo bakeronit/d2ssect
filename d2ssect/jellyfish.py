4c2d2f4c54bbfe6e6b88b4c7359b5490065d5dbf
bakeronit <bakerzj36@gmail.com>
2022-03-28 10:05:54 +1000

    add python code

--- d2ssect/jellyfish.py
+++ d2ssect/jellyfish.py
@@ -1,8 +1,19 @@
 # jellyfish count -m 19 -t 5 -s 1G -o data/jf/DI-1-1_S6.jf data/fasta/DI-1-1_S6.fasta
 
 import subprocess
+from sys import stdout
 
 def count(input,output):
 	print(input)
 	jfresult = subprocess.run(["jellyfish","count","-m 19", "-t 5", "-s 1G","-o",output,input])
 	jfresult
+
+
+def dump(input, output):
+	print(f"Dumping kmer count results from {input}")
+	dump_result = subprocess.Popen(["jellyfish","dump","-ct", input], stdout=subprocess.PIPE)
+	dump_sort = subprocess.Popen(["sort","-k1,1"],stdin=dump_result.stdout, stdout=subprocess.PIPE)
+	nkz_result = subprocess.run(["python2","v0/Kmers_2_NumbericRepresentation.py","-o", output])
+	nkz_result
+
+	
--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,16 @@
 import unittest
 import tempfile
 import filecmp
-import os
+import os, shutil
 import d2ssect.jellyfish as jellyfish
 
 ## import other functions to be tested
 
 class TestJellyfish(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.test_dir = tempfile.mkdtemp()
+        return super().setUp()
         
     def test_jfcount(self):
         inputf="data/fasta/DI-1-1_S6.fasta"
@@ -24,5 +27,19 @@ class TestJellyfish(unittest.TestCase):
         self.assertTrue(os.path.getsize(outputf),os.path.getsize(expected_output))
 
         
+    def test_jfdump(self):
+        inputf="data/jf/D1-1-1_S6.jf"
+        outputf=os.path.join(self.test_dir,"D1-1-1_S6.nkz")
+        
+        jellyfish.dump(inputf, outputf)
+        expected_output="data/nkz/DI-1-1_S6.nkz"
+
+        self.assertTrue(filecmp.cmp(outputf, expected_output))
+
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.test_dir)
+        return super().tearDown()
+
 if __name__ == '__main__':
     unittest.main()
